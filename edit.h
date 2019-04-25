d3633ba7c57c76fc670ad53babe0329f94d38a07
Aeliko <47219799+Aeliko@users.noreply.github.com>
2019-04-26 00:39:10 +0800

    Edit edit function to include editing of custom attribute

--- edit.h
+++ edit.h
@@ -1,14 +1,23 @@
 Node *edit(string choice, Node *current) {
-    string parm;
-
-    if (choice == "name") {
-        cin >> current->name;
-    }   else if (choice == "age") {
-        cin >> current->age;
-
-    }   else if (choice == "role") {
-        cin >> current->role;
-
+  string parm;
+  if (choice == "name") {
+    cin >> current->name;
+    }
+    else
+    if (choice == "age") {
+      cin >> current->age;
+    }
+    else
+    if (choice == "salary") {
+      cin >> current->salary;
+    }
+    else
+    if (choice == "role") {
+      cin >> current->role;
+    }
+    else
+    if (choice == "attributes") {
+      cin >> current->attributes;
     }
 
     // while (current != NULL) {
