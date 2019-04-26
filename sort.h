f3885366e0b33e55d27c68bef1ce52848b2ac19f
Aeliko <47219799+Aeliko@users.noreply.github.com>
2019-04-26 16:54:10 +0800

    Add sort function

---
+++ sort.h
@@ -0,0 +1,28 @@
+void sort(string attr, Node *head, Node *tail)
+Node *current = head;
+Node *end = tail;
+Node *temp = tail;
+int swapped = 1;    //flag
+while (swapped) {
+  swapped = 0;
+  while (current->next != end) {
+    if (current->next->attr > current->attr) {
+      swap(current, current->next);
+      swapped = 1;
+    }
+    if (current->next->next == end) {
+      temp = current->next;
+    }
+    current = current->next;
+  }
+  end = temp;
+}
+
+
+
+
+//two, one, three
+void swap(Node *one, Node *two) {
+  two->next = one->next;    //three
+  one->next = two;
+}
