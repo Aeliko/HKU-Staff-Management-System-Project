ebeb78bb9b671233259d69938960dbf1c6b6953d
lok2987 <48434342+lok2987@users.noreply.github.com>
2019-04-27 03:09:56 +0800

    Update edit.h

--- edit.h
+++ edit.h
@@ -1,30 +1,34 @@
-Node *edit(string choice, Node *current) {
+Node *edit(string choice, Node *current, string attribute_name) {
   string parm;
   if (choice == "name") {
+    cout << "Please enter new value: ";
     cin >> current->name;
     }
     else
     if (choice == "age") {
+        cout << "Please enter new value: ";
+
       cin >> current->age;
     }
     else
     if (choice == "salary") {
+
+        cout << "Please enter new value: ";
       cin >> current->salary;
     }
     else
     if (choice == "role") {
+
+        cout << "Please enter new value: ";
       cin >> current->role;
     }
     else
-    if (choice == "attributes") {
+    if (choice == attribute_name) {
+
+        cout << "Please enter new value: ";
       cin >> current->attributes;
     }
 
-    // while (current != NULL) {
-    //     if ((current->parm) == target) {
-    //         return current;
-    //     } else
-    //         current = current->next;
-    // }
+
     return NULL;
 }
