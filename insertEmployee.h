89515197a413ab4113b55355a433c72d7a646495
lok2987 <48434342+lok2987@users.noreply.github.com>
2019-04-27 16:53:39 +0800

    Add files via upload

--- insertEmployee.h
+++ insertEmployee.h
@@ -1,16 +1,27 @@
 void insertEmployee( Node * after, string customAttribute)
 {
     Node *employee = new Node;
+    cout << "Enter ID: ";
     cin >> employee->id;
+    cout << "Enter Name: ";
+
     cin >> employee->name;
+    cout << "Enter Age: ";
+
     cin >> employee->age;
+    cout << "Enter Salary: ";
+
     cin >> employee->salary;
+    cout << "Enter Role: ";
+
     cin >> employee->role;
+
     employee->next = NULL;
-    if (customAttribute != "No_attribute")
-    cin >> employee->attributes;
-    else
-    employee->attributes = "";
+    if (customAttribute != "No_attribute") {
+        cout << "Enter " << customAttribute << ": ";
+        cin >> employee->attributes;
+    }    else
+        employee->attributes = "";
     employee->next= after->next;
 	after->next = employee;
 
