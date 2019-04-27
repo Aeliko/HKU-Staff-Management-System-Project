819e3e974c737e5a1c9172ce6f8a26219bf0e4c7
lok2987 <48434342+lok2987@users.noreply.github.com>
2019-04-27 21:58:45 +0800

    Add files via upload

--- edit.h
+++ edit.h
@@ -7,26 +7,31 @@ Node *edit(string choice, Node *current, string attribute_name) {
     else
     if (choice == "Age") {
         cout << "Please enter new value: ";
-
-      cin >> current->age;
+        cin >> current->age;
     }
     else
     if (choice == "Salary") {
 
         cout << "Please enter new value: ";
-      cin >> current->salary;
+        cin >> current->salary;
     }
     else
     if (choice == "Role") {
 
         cout << "Please enter new value: ";
-      cin >> current->role;
+        cin >> current->role;
     }
     else
     if (choice == attribute_name) {
 
         cout << "Please enter new value: ";
-      cin >> current->attributes;
+        cin >> current->attributes;
+    }
+    else
+    if (choice == "ID") {
+        cout << "Please enter new value: ";
+        cin >> current->id;
+
     }
     else cout << "Invalid input" << endl;
 
